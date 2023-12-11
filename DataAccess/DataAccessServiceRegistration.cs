9246cd3a71d217184c62729820a7e0373417044f
bkklc <abkklc@gmail.com>
2023-12-12 00:52:08 +0300

    .

--- DataAccess/DataAccessServiceRegistration.cs
+++ DataAccess/DataAccessServiceRegistration.cs
@@ -17,6 +17,10 @@ public static class DataAccessServiceRegistration
     public static IServiceCollection AddDataAccessServices(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<TobetoContext>(options => options.UseSqlServer(configuration.GetConnectionString("Tobeto")));
+
+        services.AddScoped<ICourseDal, EfCourseDal>();
+        services.AddScoped<IRoleDal, EfRoleDal>();
+
         return services;
     }
 }
