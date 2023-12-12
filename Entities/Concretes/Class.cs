cc4c27176755fcb3f6cc54ec3be5502080a53c28
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-12-12 16:59:01 +0300

    Class entities added

--- Entities/Concretes/Class.cs
+++ Entities/Concretes/Class.cs
@@ -1,5 +1,9 @@
-﻿namespace Entities.Concretes;
+﻿using Core.Entities;
 
-public class Class
-{
+namespace Entities.Concretes;
+
+public class Class : Entity<int>
+{   
+    public string Name { get; set; }
+    public string Classroom { get; set; }
 }
