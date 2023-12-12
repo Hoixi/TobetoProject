ea6e0d7d16d3d49832e84aec0722f925a9d4b40b
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-12-12 15:11:34 +0300

    User Profile Added

--- Business/Profiles/UserProfile.cs
+++ Business/Profiles/UserProfile.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using Entities.Concretes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +12,10 @@ namespace Business.Profiles
 {
     public class UserProfile:Profile
     {
+        public UserProfile()
+        {
+            CreateMap<CreateUserRequest, User>();
+            CreateMap<User, CreateUserResponse>();
+        }
     }
 }
