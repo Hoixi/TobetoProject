77012f5d98e87b1d9b4f413f9b27d2b554fb5a7c
bkklc <abkklc@gmail.com>
2023-12-15 17:09:21 +0300

    Code Refactor

--- Entities/Concretes/User.cs
+++ Entities/Concretes/User.cs
@@ -4,7 +4,6 @@ namespace Entities.Concretes
 {
     public class User:Entity<int>
     {
-        public int Id { get; set; }
         public int RoleId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
