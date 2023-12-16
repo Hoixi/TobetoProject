6b38afc09b01a2676872a6fcecbf82f614585115
mehmetcesur <mehmetceesur@gmail.com>
2023-12-16 17:11:29 +0300

    commit

--- Entities/Concretes/Student.cs
+++ Entities/Concretes/Student.cs
@@ -9,9 +9,22 @@ namespace Entities.Concretes;
 
 public class Student : Entity<int>
 {
-    public int UserId {  get; set; }
+    public int UserId { get; set; }
     public int StudentNumber { get; set; }
     public User User { get; set; }
+    public Classroom Classroom { get; set; } // -kaldırılacak hata çıkmasın diye ekledim
 
 }
 
+public class ClassroomStudent : Entity<int>
+{
+    public int ClassroomId { get; set; }
+    public int StudentId { get; set; }
+    public Student Student { get; set; }
+    public User User { get; set; }
+}
+
+/*
+ 1 - 1 - 1
+ 2 - 1 - 2
+ */
