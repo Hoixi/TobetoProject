8f87c1e18ac42022f93f2e14347f316f50d72a32
bkklc <abkklc@gmail.com>
2023-12-16 17:14:47 +0300

    Entity added

--- Entities/Concretes/Student.cs
+++ Entities/Concretes/Student.cs
@@ -16,14 +16,6 @@ public class Student : Entity<int>
 
 }
 
-public class ClassroomStudent : Entity<int>
-{
-    public int ClassroomId { get; set; }
-    public int StudentId { get; set; }   
-    public Student Student { get; set; }
-    public User User { get; set; }
-}
-
 /*
  1 - 1 - 1
  2 - 1 - 2
