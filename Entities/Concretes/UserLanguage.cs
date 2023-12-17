63d9fa9c0b6e3a5e0df702da1537da5ea78f6c03
PC <burakugras67@gmail.com>
2023-12-17 21:34:15 +0300

    refactoring

--- Entities/Concretes/UserLanguage.cs
+++ Entities/Concretes/UserLanguage.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Entities.Concretes
 {
-    public class UserLanguages:Entity<int>
+    public class UserLanguage:Entity<int>
     {
         public int UserId { get; set; }
         public int LanguageId { get; set; }
