77a4f8f9c18e3a5185baf627e887f05ec0420e2b
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-12-17 20:48:03 +0300

    Entities(ClassroomStudent, Group, Student) added

---
+++ Entities/Concretes/ClassroomStudent.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class ClassroomStudent:Entity<int>
+    {
+        public int ClassroomGroupId { get; set; }
+        public int StudentId { get; set; }
+    }
+}
