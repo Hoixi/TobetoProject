34032348f43db44d29931b35ad8cf7cdba20a903
bkklc <abkklc@gmail.com>
2023-12-17 21:02:13 +0300

    Some entities are added

---
+++ Entities/Concretes/Education.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Education : Entity<int>
+    {
+        public int UserId { get; set; }
+        public  int EducationsDegreeId { get; set; }
+        public string SchoolName { get; set; }
+        public string Department { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime ?EndDate { get; set; }
+
+    }
+}
---
+++ Entities/Concretes/Experience.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Experience : Entity<int>
+    {
+        public int UserId { get; set; }
+        public int CityId { get; set; }
+        public string CompanyName { get; set; }
+        public string Position { get; set;}
+        public  string Sector { get; set; }
+        public DateTime StartDate { get; set;}
+        public DateTime ?EndDate { get; set;}
+    }
+}
