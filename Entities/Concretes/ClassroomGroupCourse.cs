9c70cd3f9477a86ab7ef7af95cc7a37c096b7d48
PC <burakugras67@gmail.com>
2023-12-17 20:59:25 +0300

    Add ClassroomGroupCourse, Instructor, ClassroomInstructor

---
+++ Entities/Concretes/ClassroomGroupCourse.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class ClassroomGroupCourse : Entity<int>
+    {
+        public int ClassroomGroupId { get; set; }
+        public int CourseId { get; set; }
+    }
+}
