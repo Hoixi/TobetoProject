d5d6d96c6379ead5b829808b35b35717188bd58d
PC <burakugras67@gmail.com>
2023-12-17 21:05:40 +0300

    Add LanguageLevel, UserLanguages, City

---
+++ Entities/Concretes/UserLanguages.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class UserLanguages:Entity<int>
+    {
+        public int UserId { get; set; }
+        public int LanguageId { get; set; }
+        public int LanguageLevelId { get; set; }
+    }
+}
