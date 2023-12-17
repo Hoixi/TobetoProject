183604fdfc9bff4bb1fe29ebfa730677b2a46d68
bkklc <abkklc@gmail.com>
2023-12-17 21:03:59 +0300

    Adress Entity added

---
+++ Entities/Concretes/Adress.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concretes
+{
+    public class Adress : Entity<int>
+    {
+        public int UserId { get; set; }
+        public int TownId { get; set; }
+        public int CityId { get; set; }
+        public int CountryId { get; set; }
+        public string Description { get; set; }
+    }
+}
