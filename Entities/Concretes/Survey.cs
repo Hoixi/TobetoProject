b47428e3f5e516a8049bf0e26888c82b8f8e9f86
Beyza Altıntoprak <cicurit@gmail.com>
2023-12-18 20:11:22 +0300

    New entity added

---
+++ Entities/Concretes/Survey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Core.Entities;
+
+namespace Entities.Concretes
+{
+	public class Survey:Entity<int>
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string Url { get; set; }
+	}
+}
+
