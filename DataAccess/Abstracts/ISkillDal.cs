691d15b029be2af8e9c7d3d3140c23d5b7376634
PC <burakugras67@gmail.com>
2023-12-18 19:26:41 +0300

    Add DataAcces Dal classes & interfaces

---
+++ DataAccess/Abstracts/ISkillDal.cs
@@ -0,0 +1,14 @@
+﻿using Core.DataAccess.Repositories;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstracts
+{
+    public interface ISkillDal : IRepository<Skill, int>, IAsyncRepository<Skill, int>
+    {
+    }
+}
