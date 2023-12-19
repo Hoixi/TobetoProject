04835b019abddef5e12e871412d598383e6e54bf
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-12-19 18:34:59 +0300

    CourseSubType Added

--- Entities/Concretes/Course.cs
+++ Entities/Concretes/Course.cs
@@ -7,6 +7,7 @@ public class Course : Entity<int>
     public string ImageId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public int SubTypeId { get; set; }
     public DateTime StartedDate { get; set; }
     public DateTime EndDate { get; set; }
   
