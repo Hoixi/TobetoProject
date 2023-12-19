df3a8a9aa8515d31ca3639ebbe0f97775f622145
Hoixi <furkan.kayali.1905@gmail.com>
2023-12-20 00:40:55 +0300

    User Add API

---
+++ Business/Profiles/UserProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Business.Dtos.Requests.UserRequests;
+using Business.Dtos.Responses.UserResponses;
+using Core.DataAccess.Paging;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+
+        {
+            CreateMap<CreateUserRequest, User>();
+            CreateMap<User, CreatedUserResponse>();
+
+
+            CreateMap<User, GetListUserResponse>().ReverseMap();
+            CreateMap<Paginate<User>, Paginate<GetListUserResponse>>();
+
+            CreateMap<UpdateUserRequest, User>().ForMember(dest => dest.CreatedDate, opt => opt.Ignore());
+            CreateMap<User, UpdatedUserResponse>();
+        }
+    }
+}
