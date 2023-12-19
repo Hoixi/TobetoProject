4bcb7eae424147d98db0ccf0a08dfb11b8dd281a
mehmetcesur <mehmetceesur@gmail.com>
2023-12-19 23:19:13 +0300

    Business Managers Added

---
+++ Business/Concretes/GroupManager.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Business.Abstracts;
+using Business.Dtos.Requests.GroupRequests;
+using Business.Dtos.Requests.RoleRequests;
+using Business.Dtos.Responses.GroupResponses;
+using Business.Dtos.Responses.RoleResponses;
+using Core.DataAccess.Paging;
+using DataAccess.Abstracts;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class GroupManager : IGroupService
+    {
+        IGroupDal _groupDal;
+        IMapper _mapper;
+        public GroupManager(IGroupDal groupDal, IMapper mapper)
+        {
+            _groupDal = groupDal;
+            _mapper = mapper;
+        }
+
+        
+
+
+        public async Task<CreatedGroupResponse> AddAsync(CreateGroupRequest createGroupRequest)
+        {
+            Group  group = _mapper.Map<Group>(createGroupRequest);
+            Group createdGroup = await _groupDal.AddAsync(group);
+            CreatedGroupResponse createdGroupResponse = _mapper.Map<CreatedGroupResponse>(createdGroup);
+            return createdGroupResponse;
+        }
+
+        public  async Task<Group> DeleteAsync(int id)
+        {
+            var data = await _groupDal.GetAsync(i => i.Id == id);
+            var result = await _groupDal.DeleteAsync(data);
+            return result;
+        }
+
+        public async Task<IPaginate<GetListGroupResponse>> GetAllAsync(PageRequest pageRequest)
+        {
+            var data = await _groupDal.GetListAsync(
+                index: pageRequest.PageIndex,
+                size: pageRequest.PageSize
+                );
+            var result = _mapper.Map<Paginate<GetListGroupResponse>>(data);
+            return result;
+        }
+
+        public async Task<UpdatedGroupResponse> UpdateAsync(UpdateGroupRequest updateGroupRequest)
+        {
+            var data = await _groupDal.GetAsync(i => i.Id == updateGroupRequest.Id);
+            _mapper.Map(updateGroupRequest, data);
+            data.UpdatedDate = DateTime.Now;
+            await _groupDal.UpdateAsync(data);
+            var result = _mapper.Map<UpdatedGroupResponse>(data);
+            return result;
+
+        }
+    }
+}
---
+++ Business/Concretes/ImageManager.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using Business.Abstracts;
+using Business.Dtos.Requests.GroupRequests;
+using Business.Dtos.Requests.ImageRequests;
+using Business.Dtos.Responses.GroupResponses;
+using Business.Dtos.Responses.ImageResponses;
+using Core.DataAccess.Paging;
+using DataAccess.Abstracts;
+using DataAccess.Concretes;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes;
+
+public class ImageManager : IImageService
+{
+    IImageDal _imageDal;
+    IMapper _mapper;
+
+
+    public ImageManager(IImageDal imageDal, IMapper mapper)
+    {
+        _imageDal = imageDal;
+        _mapper = mapper;
+    }
+
+    
+
+    public async Task<CreatedImageResponse> AddAsync(CreateImageRequest createImageRequest)
+    {
+        Image image = _mapper.Map<Image>(createImageRequest);
+        Image createdImage = await _imageDal.AddAsync(image);
+        CreatedImageResponse createdImageResponse = _mapper.Map<CreatedImageResponse>(createdImage);
+        return createdImageResponse;
+    }
+
+    public async Task<Image> DeleteAsync(int id)
+    {
+        var data = await _imageDal.GetAsync(i => i.Id == id);
+        var result = await _imageDal.DeleteAsync(data);
+        return result;
+    }
+
+    public async Task<IPaginate<GetListImageResponse>> GetAllAsync(PageRequest pageRequest)
+    {
+        var data = await _imageDal.GetListAsync(
+             index: pageRequest.PageIndex,
+             size: pageRequest.PageSize
+             );
+        var result = _mapper.Map<Paginate<GetListImageResponse>>(data);
+        return result;
+    }
+
+    public async Task<UpdatedImageResponse> UpdateAsync(UpdateImageRequest updateImageRequest)
+    {
+        var data = await _imageDal.GetAsync(i => i.Id == updateImageRequest.Id);
+        _mapper.Map(updateImageRequest, data);
+        data.UpdatedDate = DateTime.Now;
+        await _imageDal.UpdateAsync(data);
+        var result = _mapper.Map<UpdatedImageResponse>(data);
+        return result;
+    }
+}
