87ebd15b412e8747bbe86a7f0ce16e618834d048
Hoixi <furkan.kayali.1905@gmail.com>
2023-12-19 17:53:29 +0300

    Add Folder

---
+++ DataAccess/EntityConfigurations/CategoryConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.EntityConfigurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
