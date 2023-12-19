147802a8a61a7bc765283d39bd03c042876ec063
Beyza Altıntoprak <cicurit@gmail.com>
2023-12-20 00:25:05 +0300

    Manager Added

---
+++ Business/Concretes/UserLanguageManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using AutoMapper;
+using Business.Abstracts;
+using Business.Dtos.Requests.UserLanguageRequests;
+using Business.Dtos.Responses.UserLanguageResponses;
+using Core.DataAccess.Paging;
+using DataAccess.Abstracts;
+using DataAccess.Concretes;
+using Entities.Concretes;
+
+namespace Business.Concretes
+{
+    public class UserLanguageManager : IUserLanguageService
+    {
+        IUserLanguageDal _userLanguageDal;
+        IMapper _mapper;
+
+        public UserLanguageManager(IUserLanguageDal userLanguageDal, IMapper mapper)
+        {
+            _userLanguageDal = userLanguageDal;
+            _mapper = mapper;
+        }
+
+        public async Task<CreatedUserLanguageResponse> AddAsync(CreateUserLanguageRequest createUserLanguageRequest)
+        {
+            UserLanguage userLanguage = _mapper.Map<UserLanguage>(createUserLanguageRequest);
+            UserLanguage createdUserLanguage = await _userLanguageDal.AddAsync(userLanguage);
+            CreatedUserLanguageResponse createdUserLanguageResponse = _mapper.Map<CreatedUserLanguageResponse>(createdUserLanguage);
+            return createdUserLanguageResponse;
+        }
+
+        public async Task<UserLanguage> DeleteAsync(int id)
+        {
+            var data = await _userLanguageDal.GetAsync(i => i.Id == id);
+            var result = await _userLanguageDal.DeleteAsync(data);
+            return result;
+        }
+
+        public async Task<IPaginate<GetListUserLanguageResponse>> GetAllAsync(PageRequest pageRequest)
+        {
+            var data = await _userLanguageDal.GetListAsync(
+                index: pageRequest.PageIndex,
+                size: pageRequest.PageSize
+               );
+            var result = _mapper.Map<Paginate<GetListUserLanguageResponse>>(data);
+            return result;
+        }
+
+        public async Task<UpdatedUserLanguageResponse> UpdateAsync(UpdateUserLanguageRequest updateUserLanguageRequest)
+        {
+            var data = await _userLanguageDal.GetAsync(i => i.Id == updateUserLanguageRequest.Id);
+            _mapper.Map(updateUserLanguageRequest, data);
+            data.UpdatedDate = DateTime.Now;
+            await _userLanguageDal.UpdateAsync(data);
+            var result = _mapper.Map<UpdatedUserLanguageResponse>(data);
+            return result;
+        }
+    }
+
+}
+
---
+++ Business/Concretes/UserSkillManager.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Business.Abstracts;
+using Business.Dtos.Requests.UserSkillRequests;
+using Business.Dtos.Responses.UserSkillResponses;
+using Core.DataAccess.Paging;
+using DataAccess.Abstracts;
+using Entities.Concretes;
+
+namespace Business.Concretes
+{
+    public class UserSkillManager : IUserSkillService
+    {
+        IUserSkillDal _userSkillDal;
+        IMapper _mapper;
+
+        public UserSkillManager(IUserSkillDal userSkillDal, IMapper mapper)
+        {
+            _userSkillDal = userSkillDal;
+            _mapper = mapper;
+        }
+
+        public async Task<CreatedUserSkillResponse> AddAsync(CreateUserSkillRequest createUserSkillRequest)
+        {
+            UserSkill userSkill = _mapper.Map<UserSkill>(createUserSkillRequest);
+            UserSkill createdUserSkill = await _userSkillDal.AddAsync(userSkill);
+            CreatedUserSkillResponse createdUserSkillResponse = _mapper.Map<CreatedUserSkillResponse>(createdUserSkill);
+            return createdUserSkillResponse;
+        }
+
+        public async Task<UserSkill> DeleteAsync(int id)
+        {
+            var data = await _userSkillDal.GetAsync(i => i.Id == id);
+            var result = await _userSkillDal.DeleteAsync(data);
+            return result;
+        }
+
+        public async Task<IPaginate<GetListUserSkillResponse>> GetAllAsync(PageRequest pageRequest)
+        {
+            var data = await _userSkillDal.GetListAsync(
+                index: pageRequest.PageIndex,
+                size: pageRequest.PageSize
+               );
+            var result = _mapper.Map<Paginate<GetListUserSkillResponse>>(data);
+            return result;
+        }
+
+        public async Task<UpdatedUserSkillResponse> UpdateAsync(UpdateUserSkillRequest updateUserSkillRequest)
+        {
+            var data = await _userSkillDal.GetAsync(i => i.Id == updateUserSkillRequest.Id);
+            _mapper.Map(updateUserSkillRequest, data);
+            data.UpdatedDate = DateTime.Now;
+            await _userSkillDal.UpdateAsync(data);
+            var result = _mapper.Map<UpdatedUserSkillResponse>(data);
+            return result;
+        }
+    }
+
+}
+
