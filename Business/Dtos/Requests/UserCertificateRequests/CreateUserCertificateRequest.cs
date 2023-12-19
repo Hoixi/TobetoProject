2c0faadfc9a669b3ea324d968444b73abd57e113
burakugras <burakugras67@gmail.com>
2023-12-19 20:33:59 +0300

    Add UserCertificate DTOs

---
+++ Business/Dtos/Requests/UserCertificateRequests/CreateUserCertificateRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dtos.Requests.UserCertificateRequests
+{
+    public class CreateUserCertificateRequest
+    {
+        public int UserId { get; set; }
+        public int CertificateId { get; set; }
+    }
+}
---
+++ Business/Dtos/Responses/UserCertificateResponses/UpdatedUserCertificateResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Dtos.Responses.UserCertificateResponses
+{
+    public class UpdatedUserCertificateResponse
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CertificateId { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
