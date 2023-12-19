1a511384470cb11635ead2cb6cdd02867fa945cb
Cemal Taşkın <77429146+CemalTkn@users.noreply.github.com>
2023-12-19 22:35:03 +0300

    Business abstract(CategoryManager, CertificateManager, CityManager) added

---
+++ Business/Concretes/CertificateManager.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Business.Abstracts;
+using Business.Dtos.Requests.AddressRequests;
+using Business.Dtos.Requests.CertificateRequests;
+using Business.Dtos.Responses.AddressResponses;
+using Business.Dtos.Responses.CertificateResponses;
+using Core.DataAccess.Paging;
+using DataAccess.Abstracts;
+using DataAccess.Concretes;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class CertificateManager : ICertificateService
+    {
+        ICertificateDal _certificateDal;
+        IMapper _mapper;
+
+        public CertificateManager(ICertificateDal certificateDal, IMapper mapper)
+        {
+            _certificateDal = certificateDal;
+            _mapper = mapper;
+        }
+
+        public async Task<CreatedCertificateResponse> AddAsync(CreateCertificateRequest createCertificateRequest)
+        {
+            Certificate certificate = _mapper.Map<Certificate>(createCertificateRequest);
+            Certificate createdCertificate = await _certificateDal.AddAsync(certificate);
+            CreatedCertificateResponse createdCertificateResponse = _mapper.Map<CreatedCertificateResponse>(createdCertificate);
+            return createdCertificateResponse;
+        }
+
+        public async Task<Certificate> DeleteAsync(int id)
+        {
+            var data = await _certificateDal.GetAsync(i => i.Id == id);
+            var result = await _certificateDal.DeleteAsync(data);
+            return result;
+        }
+
+        public async Task<IPaginate<GetListCertificateResponse>> GetAllAsync(PageRequest pageRequest)
+        {
+            var data = await _certificateDal.GetListAsync(
+                index: pageRequest.PageIndex,
+                size: pageRequest.PageSize
+               );
+            var result = _mapper.Map<Paginate<GetListCertificateResponse>>(data);
+            return result;
+        }
+
+        public async Task<UpdatedCertificateResponse> UpdateAsync(UpdateCertificateRequest updateCertificateRequest)
+        {
+            var data = await _certificateDal.GetAsync(i => i.Id == updateCertificateRequest.Id);
+            _mapper.Map(updateCertificateRequest, data);
+            data.UpdatedDate = DateTime.Now;
+            await _certificateDal.UpdateAsync(data);
+            var result = _mapper.Map<UpdatedCertificateResponse>(data);
+            return result;
+        }
+    }
+}
---
+++ Business/Concretes/CityManager.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Business.Abstracts;
+using Business.Dtos.Requests.AddressRequests;
+using Business.Dtos.Requests.CityRequests;
+using Business.Dtos.Responses.AddressResponses;
+using Business.Dtos.Responses.CityResponses;
+using Core.DataAccess.Paging;
+using DataAccess.Abstracts;
+using DataAccess.Concretes;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concretes
+{
+    public class CityManager : ICityService
+    {
+        ICityDal _cityDal;
+        IMapper _mapper;
+
+        public CityManager(ICityDal cityDal, IMapper mapper)
+        {
+            _cityDal = cityDal;
+            _mapper = mapper;
+        }
+
+        public async Task<CreatedCityResponse> AddAsync(CreateCityRequest createCityRequest)
+        {
+            City city = _mapper.Map<City>(createCityRequest);
+            City createdCity = await _cityDal.AddAsync(city);
+            CreatedCityResponse createdCityResponse = _mapper.Map<CreatedCityResponse>(createdCity);
+            return createdCityResponse;
+        }
+
+        public async Task<City> DeleteAsync(int id)
+        {
+            var data = await _cityDal.GetAsync(i => i.Id == id);
+            var result = await _cityDal.DeleteAsync(data);
+            return result;
+        }
+
+        public async Task<IPaginate<GetListCityResponse>> GetAllAsync(PageRequest pageRequest)
+        {
+            var data = await _cityDal.GetListAsync(
+                index: pageRequest.PageIndex,
+                size: pageRequest.PageSize
+               );
+            var result = _mapper.Map<Paginate<GetListCityResponse>>(data);
+            return result;
+        }
+
+        public async Task<UpdatedCityResponse> UpdateAsync(UpdateCityRequest updateCityRequest)
+        {
+            var data = await _cityDal.GetAsync(i => i.Id == updateCityRequest.Id);
+            _mapper.Map(updateCityRequest, data);
+            data.UpdatedDate = DateTime.Now;
+            await _cityDal.UpdateAsync(data);
+            var result = _mapper.Map<UpdatedCityResponse>(data);
+            return result;
+        }
+    }
+}
