9bf456aef9cd35fb12c721d7b0f1a5b837b82007
Hoixi <furkan.kayali.1905@gmail.com>
2023-12-20 16:51:18 +0300

    User API

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using Business.Abstracts;
 using Business.Dtos.Requests.UserRequests;
+using Core.DataAccess.Paging;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,6 +21,25 @@ namespace WebAPI.Controllers
         public async Task<IActionResult> Add([FromQuery]CreateUserRequest createUserRequest)
         {
             var result = await _userService.AddAsync(createUserRequest);
+            return Ok(result);
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll([FromQuery]PageRequest pageRequest)
+        {
+            var result = await _userService.GetAllAsync(pageRequest);
+            return Ok(result);
+        }
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update([FromQuery] UpdateUserRequest updateUserRequest)
+        {
+            var result = await _userService.UpdateAsync(updateUserRequest);
+            return Ok(result);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromQuery] int Id)
+        {
+            var result = await _userService.DeleteAsync(Id);
             return Ok(result);
         }
     }
