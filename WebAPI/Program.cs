867eaedd5d38b17a75fb799107ab3c66a954eebf
Hoixi <furkan.kayali.1905@gmail.com>
2024-01-08 21:21:41 +0300

    Program.cs Swagger OperationId

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -12,7 +12,10 @@ builder.Services.AddDataAccessServices(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["action"]}");
+});
 
 
 var app = builder.Build();
