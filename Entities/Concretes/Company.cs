6c4897c715b6239d43b31fefb26c3de760864503
CemalTkn <cemaltaskin17@gmail.com>
2024-01-17 11:49:43 +0300

    CourseCompany Join Added

--- Entities/Concretes/Company.cs
+++ Entities/Concretes/Company.cs
@@ -7,6 +7,7 @@ namespace Entities.Concretes
 	{
 		public string Name { get; set; }
 		public string Url { get; set; }
-	}
+        public List<CourseCompany> CourseCompanies { get; set; }
+    }
 }
 
