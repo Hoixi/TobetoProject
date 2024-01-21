fc30ea8296b3970738955a1bdaf012f792fb221c
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2024-01-21 15:43:13 +0300

    business rules & validations

--- Business/Concretes/AuthManager.cs
+++ Business/Concretes/AuthManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstracts;
 using Business.Dtos.Requests.UserRequests;
+using Business.Rules;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.Entities.Concretes;
 using Core.Utilities.Results;
 using Core.Utilities.Security.Hashing;
@@ -19,11 +22,13 @@ namespace Business.Concretes
     {
         private IUserService _userService;
         private ITokenHelper _tokenHelper;
+        private UserBusinessRules _userBusinessRules;
 
-        public AuthManager(IUserService userService, ITokenHelper tokenHelper)
+        public AuthManager(IUserService userService, ITokenHelper tokenHelper, UserBusinessRules userBusinessRules)
         {
             _userService = userService;
             _tokenHelper = tokenHelper;
+            _userBusinessRules = userBusinessRules;
         }
 
         public IDataResult<AccessToken> CreateAccessToken(UserBase user)
@@ -49,8 +54,10 @@ namespace Business.Concretes
             return new SuccessDataResult<UserBase>(userToCheck, "Tamam");
         }
 
-        public IDataResult<UserBase> Register(UserForRegisterRequest userForRegisterDto, string password)
+        [ValidationAspect(typeof(RegisterValidator))]
+        public async Task<IDataResult<UserBase>> Register(UserForRegisterRequest userForRegisterDto, string password)
         {
+            await _userBusinessRules.UserShouldNotExistsWithSameEmail(userForRegisterDto.Email);
             byte[] passwordHash, passwordSalt;
             HashingHelper.CreatePasswordHash(password, out passwordHash, out passwordSalt);
             var user = new UserBase
@@ -61,7 +68,7 @@ namespace Business.Concretes
                 PasswordHash = passwordHash,
                 PasswordSalt = passwordSalt,
             };
-            _userService.Add(user);
+            await _userService.AddAsync(user);
             return new SuccessDataResult<UserBase>(user, "Tamam");
         }
 
---
+++ Business/ValidationRules/FluentValidation/RegisterValidator.cs
@@ -0,0 +1,19 @@
+﻿using Business.Dtos.Requests.UserRequests;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class RegisterValidator : AbstractValidator<UserForRegisterRequest>
+    {
+        public RegisterValidator()
+        {
+            RuleFor(i => i.Email).EmailAddress().WithMessage("E-posta düzgün girilmedi.");
+            RuleFor(i => i.Password).MinimumLength(5).WithMessage("Şifre en az 5 haneli olmalıdır.");
+        }
+    }
+}
